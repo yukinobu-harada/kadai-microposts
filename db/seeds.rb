f8890a04782e9150d7ddcbef773f3e7258e7a67c
chaki.da-hara <yukiharada2412@gmail.com>
2020-09-25 10:37:19 +0000

    user signup

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+(1..5).each do |number|
+  User.create(name: 'jiro ' + number.to_s, email: 'JiRo'+number.to_s+'@TechAcademy.jp', password: 'jiro' + number.to_s)
+end
